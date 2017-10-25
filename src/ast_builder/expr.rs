32471a5ebafa5b183a010a39777ea103bfc6ae7f
Lukáš Hozda <luk.hozda@gmail.com>
2017-10-25 22:11:23 +0200

    start plotting expression

---
+++ src/ast_builder/expr.rs
@@ -0,0 +1,56 @@
+use parser::Rule;
+use ast_builder::{
+	BuilderError,
+	Pair,
+	BuildResult
+};
+
+pub trait Expression
+{
+	fn get_any(&self) -> Box<Any>;
+	fn eat_any(self) -> Box<Any>;
+	fn get_type(&self) -> ExpressionType;
+}
+
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+pub enum ExpressionType
+{
+	// compatibility layer, lol
+	Identifier,
+	Number,
+	StringLit,
+	CharLit,
+
+	// general expression
+	PrimaryExpression,
+	PrefixExpression,
+	FunctionCall,
+	Cast,
+	UnaryExpression,
+	MultiplicativeExpression, // also modulo and divide ofc
+	AdditionExpression, // also subtraction
+	ShiftExpression,
+	RelationExpression,
+	EquivalenceExpression,
+	BitWiseExpression,
+	BooleanExpression,
+	ConditionalExpression,
+	AssignmentExpression,
+	ConstantExpression,
+	Expression, // top-level
+}
+
+// primary expression
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+pub enum PrimaryExpression
+{
+	pub name: String,
+	pub alias: Option<String>,
+	local: bool,
+}
+
+
+
+pub fn parse_import(input: Pair) -> BuildResult<Expression>
+{
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate pest;
+extern crate ansi_term;
 #[macro_use] extern crate pest_derive;
 
+pub mod util;
 pub mod parser;
 pub mod ast_builder;
 
